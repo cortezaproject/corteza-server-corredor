0c1753d51327ed1f27213a56dd31c61637262c40
Denis Arh <denis.arh@gmail.com>
2019-07-31 08:57:23 +0200

    Improve gRPC server shutdown handling

--- src/grpc-server/index.js
+++ src/grpc-server/index.js
@@ -5,9 +5,29 @@ import logger from '../logger'
 export default ({ addr } = {}) => {
   const server = new grpc.Server()
 
+  const handle = () => {
+    // Override signal handler with more severe approach
+    process.on('SIGINT', () => {
+      logger.warn(`forcing gRPC server to stop`)
+      server.forceShutdown()
+    })
+
+    logger.debug(`trying to stop gRPC server`)
+    server.tryShutdown(() => {
+      logger.info(`gRPC server stopped`)
+    })
+  }
+
+  process.on('SIGINT', handle);
+  process.on('SIGTERM', handle);
+
   mountServices(server)
 
-  server.bind(addr, grpc.ServerCredentials.createInsecure())
+  if (server.bind(addr, grpc.ServerCredentials.createInsecure()) === 0) {
+    logger.error(`could not bind gRPC server to ${addr}`)
+    return
+  }
+
   logger.info(`gRPC server running at ${addr}`)
   server.start()
 }
