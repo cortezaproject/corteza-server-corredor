b1673cb33b7cc7d180d96b4ef7e316fa28c76756
Denis Arh <denis.arh@gmail.com>
2020-01-03 02:42:51 +0100

    Simplify script loading

--- src/scripts/server/maker.ts
+++ src/scripts/server/maker.ts
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-ts-ignore */
-import { promises as fs } from 'fs'
-import { Script, ScriptFn } from './types'
+import { Script } from './types'
 
 /**
  * Populates & returns script object
@@ -8,62 +6,55 @@ import { Script, ScriptFn } from './types'
  * @param {string} filepath
  * @param {string} basepath
  */
-export async function MakeScripts (filepath: string, basepath: string): Promise<Script[]> {
-  return fs.readFile(filepath).then((source): Script[] => {
-    // Trim off leading path
-    // @todo we trim too much of leading path
+export function MakeScripts (filepath: string, basepath: string): Script[] {
+  const ss: Script[] = []
+  const filename = filepath.substring(basepath.length + 1)
+  let module: {[_: string]: unknown}
 
-    const ss: Script[] = []
-    const filename = filepath.substring(basepath.length + 1)
-    let module: {[_: string]: unknown}
+  try {
+    // We'll use require instead of import
+    // because we need more control over cache (invalidation)
 
-    try {
-      // We'll use require instead of import
-      // because we need more control over cache (invalidation)
+    // Remove from cache & (re)require the script
+    delete require.cache[require.resolve(filepath)]
 
-      // Remove from cache & (re)require the script
-      delete require.cache[require.resolve(filepath)]
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    module = require(filepath)
+  } catch (e) {
+    console.error(e)
+    return [{
+      name: filename,
+      errors: [e.toString()]
+    }]
+  }
 
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      module = require(filepath)
-    } catch (e) {
-      console.error(e)
-      return [{
-        name: filename,
-        errors: [e.toString()]
-      }]
+  for (const name in module) {
+    if (!Object.prototype.hasOwnProperty.call(module, name)) {
+      continue
     }
 
-    for (const name in module) {
-      if (!Object.prototype.hasOwnProperty.call(module, name)) {
-        continue
-      }
-
-      let script: Script = {
-        // Make sure we can find our default exports
-        name: name === 'default' ? filename : name
-      }
-
-      if (typeof module[name] === 'object') {
-        script = { ...script, ...(module[name] as Script) }
-      }
-
-      ss.push(script)
+    let script: Script = {
+      // Make sure we can find our default exports
+      name: name === 'default' ? filename : name
     }
 
-    return ss
-  })
+    console.log(module, name, typeof module[name])
+    if (typeof module[name] === 'object') {
+      script = { ...script, ...(module[name] as Script) }
+    }
+
+    ss.push(script)
+  }
+
+  return ss
 }
 
 export async function Maker (gen: AsyncGenerator<string>, basepath: string): Promise<Script[]> {
-  const pp: Promise<Script[]>[] = []
+  const pp: Script[] = []
 
   for await (const scriptLocation of gen) {
-    pp.push(MakeScripts(scriptLocation, basepath))
+    pp.push(...MakeScripts(scriptLocation, basepath))
   }
 
-  return Promise.all(pp).then(
-    // @ts-ignore
-    set => set.flat()
-  )
+  return pp
 }
