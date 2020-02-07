da3f57e8600a6dea4c841b415342f17321af242a
Denis Arh <denis.arh@gmail.com>
2020-02-07 19:26:34 +0100

    Remove @todo from responsibility list

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ import {
  *
  *  1. @todo load remote (git-repo) scripts (if not local)
  *  2. watch and reload dependencies (if src/package.json exists)
- *  3. @todo watch frontend-script changes, run tests (if any) and (re)bundle scripts
+ *  3.  watch frontend-script changes, run tests (if any) and (re)bundle scripts
  *  4. watching backend-script changes and (re)link them to gRPC service
  *     @todo run tests?
  *  5. start gRPC server
